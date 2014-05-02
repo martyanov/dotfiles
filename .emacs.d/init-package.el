a73023da91ad3d8cc7dc44f52872f8bd5e2a251e
Andrey Martyanov <andrey@martyanov.com>
2014-05-02 19:59:01 +0300

    Initial Emacs setup.

---
+++ .emacs.d/init-package.el
@@ -0,0 +1,67 @@
+;;; init-package.el --- Package management and tools
+
+;;; Commentary:
+
+;; Setup package manager and default packages.
+
+;;; Code:
+
+(require 'package)
+
+;; Packages installed by default
+(defvar default-packages
+  '(ace-jump-mode
+    ace-jump-buffer
+    ack-and-a-half
+    browse-kill-ring
+    ctags-update
+    dash
+    diminish
+    direx
+    epl
+    erlang
+    expand-region
+    flx
+    flx-ido
+    flycheck
+    go-mode
+    haskell-mode
+    ido-ubiquitous
+    jinja2-mode
+    lua-mode
+    midnight
+    monokai-theme
+    rainbow-delimiters
+    rainbow-mode
+    smartparens
+    smex
+    undo-tree
+    yaml-mode
+    yasnippet))
+
+;; Add melpa package repository and initialize package database
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+
+;; Store package cache in ~/.emacs.d/elpa
+(setq package-user-dir (expand-file-name "elpa" "~/.emacs.d"))
+
+;; Refresh packages list if not already
+(unless (file-exists-p package-user-dir)
+  (package-refresh-contents))
+
+(defun install-packages (packages)
+  "Install packages, provided in PACKAGES list."
+  (mapc #'install-package packages))
+
+(defun install-package (package)
+  "Install provided PACKAGE."
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+;; Install default packages
+(install-packages default-packages)
+
+(provide 'init-package)
+;;; init-package.el ends here
---
+++ .emacs.d/init.el
@@ -0,0 +1,45 @@
+;;; init.el --- Custom Emacs configuration
+
+;;; Commentary:
+
+;; Emacs uses this file as an entry point.
+
+;;; Code:
+
+(add-to-list 'load-path "~/.emacs.d/")
+
+;; Core requirements
+(require 'init-package)
+(require 'init-ui)
+
+;; General settings
+(setq backup-directory-alist `((".*" . "/tmp/")))
+(setq auto-save-default nil)
+
+;; Editor settings
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 8)
+(global-auto-revert-mode t)
+(delete-selection-mode t)
+(global-hl-line-mode t)
+
+;; Undo tree mode
+(global-undo-tree-mode t)
+(diminish 'undo-tree-mode)
+
+;; Whitespace mode configuration
+(global-whitespace-mode t)
+(setq whitespace-line-column 80)
+(setq whitespace-style '(empty face tabs lines-tail trailing))
+
+;; Kill old buffers
+(require 'midnight)
+
+;; Increase selected region by semantic units
+(require 'expand-region)
+
+;; Flycheck mode
+(global-flycheck-mode t)
+(diminish 'flycheck-mode)
+
+;;; init.el ends here

d898072297c9e3c0cc02e2dd0fad7af33904508d
Andrey Martyanov <andrey@martyanov.com>
2019-04-20 15:55:32 +0500

    Remove obsolete bash aliases

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-alias cask='brew cask'
 alias e='emacsclient -t'
 alias ec='emacsclient -c'
 alias g='git'
 alias grep='grep --color=auto'
 alias map='xargs -n1'
 alias reload='exec $SHELL -l'
-alias rmds='find . -name "*.DS_Store" -exec rm -rf {} \;'
-alias rmpyc='find . -name "*.pyc" -exec rm -rf {} \;'
 alias dockerkillall='docker kill $(docker ps -q)'

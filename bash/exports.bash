653a74178109047de76a66527d00c4e1cf8cfcf1
Andrey Martyanov <andrey@martyanov.com>
2024-08-06 23:54:36 +0500

    Remove obsolete PATH export

--- bash/exports.bash
+++ bash/exports.bash
@@ -5,7 +5,7 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # Setup general executable file paths
-export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
 # Pretty terminal colors
 export TERM=screen-256color
